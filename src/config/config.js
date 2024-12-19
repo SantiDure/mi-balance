6a70ae3d55cd356d3560a2accaa3289ae7a5bda3
SantiDure <santidure2002@gmail.com>
2024-12-19 08:52:21 -0300

    env3

--- src/config/config.js
+++ src/config/config.js
@@ -6,5 +6,5 @@ export const MONGODB_CNX_STR = process.env.MONGODB_CNX_STR
 export const JWT_SECRET=process.env.JWT_SECRET
 export const NODE_ENV=process.env.NODE_ENV
 export const LOCAL_URL=process.env.LOCAL_URL
-export const VERCEL_URL="https://mi-balance-client-2a7phh0yq-santidures-projects.vercel.app"
+export const VERCEL_URL=process.env.VERCEL_URL
 
