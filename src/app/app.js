685d567918cbe2a9c2649a57d7a91b9b49396b91
SantiDure <santidure2002@gmail.com>
2024-12-19 09:29:48 -0300

    env4

--- src/app/app.js
+++ src/app/app.js
@@ -6,49 +6,47 @@ import passport from "passport";
 import passportConfig from '../middlewares/passport.config.js';
 import cookieParser from "cookie-parser";
 import cors from "cors"
-export class Server{
-    server;
-    constructor(){
-        this.port = PORT,
-        this.app = express()
-        const allowedOrigins = [
-          LOCAL_URL,  
-          VERCEL_URL,
-        ];
-          
-        this.app.use(cors({
-          origin: function (origin, callback) {
-            if (!origin || allowedOrigins.includes(origin)) {
-              callback(null, true);
-            } else {
-              callback(new Error("Not allowed by CORS"));
-            }
-          },
-          methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-          credentials: true,
-        }));
+export class Server {
+  server;
+  constructor() {
+    this.port = PORT,
+      this.app = express()
+    const allowedOrigins = [LOCAL_URL, VERCEL_URL];
 
-        this.app.use(express.json());
-        this.app.use(express.urlencoded({ extended: true }));
-        this.app.use(cookieParser())
-        this.app.use((err, req, res, next) => {
-            console.error(err.stack);
-            res.status(500).send(err.stack);
-          });
-        this.app.use("/api",apiRouter); 
-        passportConfig(passport);
-        this.app.use(passport.initialize());
-    }
-    connect() {
-        return new Promise((resolve, reject) => {
-          this.server = this.app.listen(this.port, () => {
-            console.log(process.env)
-            resolve(console.log(`listen ${PORT} - ${NODE_ENV}`));
-          });
-        });
-      }
-      async connectDb() {
-        await mongoose.connect(MONGODB_CNX_STR, { socketTimeoutMS: 45_000 });
-        return console.log(`DB conectada`);
-      }
+    this.app.use(cors({
+      origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+          console.log(allowedOrigins)
+          callback(null, true);
+        } else {
+          callback(new Error("Not allowed by CORS"));
+        }
+      },
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+      credentials: true,
+    }));
+
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(cookieParser())
+    this.app.use((err, req, res, next) => {
+      console.error(err.stack);
+      res.status(500).send(err.stack);
+    });
+    this.app.use("/api", apiRouter);
+    passportConfig(passport);
+    this.app.use(passport.initialize());
+  }
+  connect() {
+    return new Promise((resolve, reject) => {
+      this.server = this.app.listen(this.port, () => {
+
+        resolve(console.log(`listen ${PORT} - ${NODE_ENV}`));
+      });
+    });
+  }
+  async connectDb() {
+    await mongoose.connect(MONGODB_CNX_STR, { socketTimeoutMS: 45_000 });
+    return console.log(`DB conectada`);
+  }
 }
