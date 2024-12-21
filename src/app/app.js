8f52a8f22a8dc585d57d8cc58856607e6469250b
SantiDure <santidure2002@gmail.com>
2024-12-21 17:42:58 -0300

    carpeta api9

--- src/app/app.js
+++ src/app/app.js
@@ -14,7 +14,7 @@ export class Server {
     const allowedOrigins = [LOCAL_URL, VERCEL_URL];
 
     this.app.use(cors({
-      origin:allowedOrigins,
+      origin:"https://mi-balance.vercel.app",
       credentials:true
     }))
     // this.app.options("*", cors());
